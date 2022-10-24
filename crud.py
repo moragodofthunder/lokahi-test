68e22fca242e5782e62d0ee7fccab3d69f4d5ebb
Mora <moragodofthunder@gmail.com>
2022-10-24 14:13:31 -0700

    Initial commit.

---
+++ crud.py
@@ -0,0 +1,10 @@
+"""CRUD operations."""
+#Change this after adding classes to model.py
+from model import db, User, Movie, Rating, connect_to_db
+
+
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
---
+++ model.py
@@ -0,0 +1,18 @@
+"""Models for Lokahi web app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
